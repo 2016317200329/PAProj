ae6f530b95122a82ecafbd2100d3e0eb7d5d4b1e
Administrator <360600842@qq.com>
2023-02-22 15:14:55 +0800

    230222-update

--- MLP/mydataset.py
+++ MLP/mydataset.py
@@ -14,7 +14,7 @@ import numpy as np
 from torch.utils.data import DataLoader, SubsetRandomSampler
 
 class myDataset(Dataset):
-    def __init__(self, train_path, target_path, key_path):
+    def __init__(self, train_path, target_path, key_path, metric_path):
         """
         读取data path
 
@@ -27,8 +27,9 @@ class myDataset(Dataset):
         # target_path里有全部的target data地址
         self.target_all_path = os.listdir(target_path)
         self.key_path = key_path
+        self.metric_path = metric_path
 
-        # Rescale range
+        # [Obsolete] Rescale range
         self.a = 0
         self.b = 255
 
@@ -41,7 +42,7 @@ class myDataset(Dataset):
 
     def rescale(self,data,a,b) :
         """
-        对data重新rescale to range of [a,b]
+        [Obsolete] 对data重新rescale to range of [a,b]
 
         :param data: [Chanel,Dim]. Data to be rescaled
         :param a: Lower range
@@ -59,25 +60,34 @@ class myDataset(Dataset):
 
     def __getitem__(self, index):
         """
-        返回第index个 training data，target data
+        返回第index个 training data，target data, setting data
         :param index: 文件序
         :return: train_data, target_data
         """
         # print(f"target file is {self.target_all_path[index]}")
         train_path_i_path = os.path.join(self.train_root_path,self.train_all_path[index])
         target_path_i_path = os.path.join(self.target_root_path,self.target_all_path[index])
+
         train_df = pd.read_csv(train_path_i_path,encoding="utf-8")
         target_df = pd.read_csv(target_path_i_path,encoding="utf-8")
+        settings = pd.read_csv(self.key_path,encoding="utf-8")
+        metric = pd.read_csv(self.metric_path,encoding="utf-8")
+
+        settings_df = settings.iloc[index,:]
+        metric_df = metric.iloc[index,:]
+
         # print("dtrain_df shape",train_df.shape)   （3，300）
 
-        # Rescale
+        # [Obsolete] Rescale
         # train_df.iloc[0:2,:] = self.rescale(train_df.iloc[0:2,:],self.a,self.b)
 
         # Transform into numpy (not tensor!)
         train_data = np.array(train_df.values,dtype=float)
         target_data = np.array(target_df.values,dtype=float)
+        settings_data = np.array(settings_df.values,dtype=float)
+        metric_data = np.array(metric_df.values,dtype=float)
 
-        return train_data, target_data
+        return train_data, target_data,settings_data, metric_data
 
 
 ######################### TEST USE ########################
@@ -90,19 +100,22 @@ class myDataset(Dataset):
 #
 # # target data
 #
-# target_path = r"../data/targets"
+# target_path = r"../data/targets_5"
 # # data keys (for target)
 # data_key_path = r"../data/target_datakey.csv"
+# # NLL metric
+# NLL_metric_path = "../data/NLL_metric_GT.csv"
 #
 # if __name__ == '__main__':
-#     dataset = myDataset(train_path, target_path, data_key_path)
+#     dataset = myDataset(train_path, target_path, data_key_path,NLL_metric_path)
 #     # print(type(dataset))  # <class '__main__.myDataset'>
 #     # train_data, target_data = dataset.__getitem__(2)
 #     # print(train_data,target_data.shape)
 #
 #     for i in range(1000,1001):
-#         train_data, target_data = dataset.__getitem__(i)
-#         # print(i,train_data)
+#         train_data, target_data,settings_data,metric_df = dataset.__getitem__(i)
+#         print(i,metric_df)
+#
 #         # assert train_data.shape==(3,100),"AAA"
 #
 #     # for data in train_loader:
