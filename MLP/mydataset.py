683686d4895fe6cffcb8d935cdd619484b53c503
Administrator <360600842@qq.com>
2023-02-27 10:13:51 +0800

    230227-update

--- MLP/mydataset.py
+++ MLP/mydataset.py
@@ -14,18 +14,20 @@ import numpy as np
 from torch.utils.data import DataLoader, SubsetRandomSampler
 
 class myDataset(Dataset):
-    def __init__(self, train_path, target_path, key_path, metric_path):
+    def __init__(self, train_path, target_path_metric, target_path_loss, key_path, metric_path):
         """
         读取data path
-
+        target_path_metric: 计算metric用的original data
+        target_path_loss： 计算loss / training用的data
         """
         self.train_root_path = train_path
-        self.target_root_path = target_path
+        self.target_metric_root_path = target_path_metric
+        self.target_loss_root_path = target_path_loss
 
         # all_path里有全部的data地址作为list
         self.train_all_path = os.listdir(train_path)
         # target_path里有全部的target data地址
-        self.target_all_path = os.listdir(target_path)
+        self.target_all_path = os.listdir(target_path_metric)
         self.key_path = key_path
         self.metric_path = metric_path
 
@@ -66,10 +68,13 @@ class myDataset(Dataset):
         """
         # print(f"target file is {self.target_all_path[index]}")
         train_path_i_path = os.path.join(self.train_root_path,self.train_all_path[index])
-        target_path_i_path = os.path.join(self.target_root_path,self.target_all_path[index])
+        target_loss_path_i_path = os.path.join(self.target_loss_root_path,self.target_all_path[index])
+        target_metric_i_path = os.path.join(self.target_metric_root_path,self.target_all_path[index])
 
         train_df = pd.read_csv(train_path_i_path,encoding="utf-8")
-        target_df = pd.read_csv(target_path_i_path,encoding="utf-8")
+        target_loss_df = pd.read_csv(target_loss_path_i_path,encoding="utf-8")
+        target_metric_df = pd.read_csv(target_metric_i_path,encoding="utf-8")
+
         settings = pd.read_csv(self.key_path,encoding="utf-8")
         metric = pd.read_csv(self.metric_path,encoding="utf-8")
 
@@ -83,11 +88,13 @@ class myDataset(Dataset):
 
         # Transform into numpy (not tensor!)
         train_data = np.array(train_df.values,dtype=float)
-        target_data = np.array(target_df.values,dtype=float)
+        target_loss_data = np.array(target_loss_df.values,dtype=float)
+        target_metric_data = np.array(target_metric_df.values,dtype=float)
+
         settings_data = np.array(settings_df.values,dtype=float)
         metric_data = np.array(metric_df.values,dtype=float)
 
-        return train_data, target_data,settings_data, metric_data
+        return train_data, target_metric_data, target_loss_data, settings_data, metric_data
 
 
 ######################### TEST USE ########################
